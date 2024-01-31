ea6debdafa4761605b30e9f2ba728aceff6ba680
nn-fiha <jucse28.361@gmail.com>
2024-02-01 00:28:13 +0600

    Div 4

---
+++ Codeforces/A. Medium Number.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t,i,ans,n;
+
+    cin>>t;
+    while(t--)
+    {
+        int a[3];
+        for(i=0; i<3; i++)
+        {
+            cin>>a[i];
+        }
+        sort(a,a+3);
+
+            cout<<a[1]<<endl;
+
+
+    }
+
+
+
+}
