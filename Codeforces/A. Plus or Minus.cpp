4464a864ac3a359cec2e67dee3aeab5106b89d40
nn-fiha <jucse28.361@gmail.com>
2024-02-01 00:29:35 +0600

    Div 4

---
+++ Codeforces/A. Plus or Minus.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t,i,ans,a,b,c;
+
+    cin>>t;
+    while(t--)
+    {
+       cin>>a>>b>>c;
+
+        if(a+b==c)
+        {
+            cout<<"+"<<endl;
+
+        }
+        else if(a-b==c){
+                cout<<"-"<<endl;
+
+            }
+    }
+
+
+
+}
