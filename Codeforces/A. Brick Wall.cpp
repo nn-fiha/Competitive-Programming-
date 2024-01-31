bb96a0ac7cdb243206e8c1e85ff911ff324ab8e2
nn-fiha <jucse28.361@gmail.com>
2024-02-01 00:23:52 +0600

    Div 2

---
+++ Codeforces/A. Brick Wall.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t,i,ans,n,m;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>m;
+        ans=n*(m/2);
+        cout<<ans<<endl;
+    }
+
+}
