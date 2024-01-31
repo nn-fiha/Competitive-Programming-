49f11d001821ca4c2d065badc458c6435a04e4bd
nn-fiha <jucse28.361@gmail.com>
2024-02-01 00:26:55 +0600

    Div 4

---
+++ Codeforces/A. Codeforces Checking.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t,i;
+    char ch;
+
+    cin>>t;
+    while(t--)
+    {
+       cin>>ch;
+
+        if(ch=='c'||ch=='o'||ch=='d'||ch=='e'||ch=='f'||ch=='r'||ch=='s')
+        {
+            cout<<"YES"<<endl;
+
+        }
+        else{
+                cout<<"NO"<<endl;
+
+            }
+    }
+
+
+
+}
