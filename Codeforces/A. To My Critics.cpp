6b9b5771d96440769557f58b18235fab3d0af697
nn-fiha <jucse28.361@gmail.com>
2024-02-01 00:29:03 +0600

    Div 4

---
+++ Codeforces/A. To My Critics.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t,i,ans,n;
+
+    cin>>t;
+    while(t--)
+    {
+        int a[3];
+        for(i=0; i<3; i++)
+        {
+            cin>>a[i];
+        }
+        sort(a,a+3);
+        if(a[1]+a[2]>=10)
+        {
+            cout<<"YES"<<endl;
+
+        }
+        else{
+                cout<<"NO"<<endl;
+
+            }
+    }
+
+
+
+}
